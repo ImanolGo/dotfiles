bfe78c2d98eeebaf7effd4217443e0809e203aa3
Imanol Gomez <imanol@tuta.io>
2023-11-02 20:47:17 +0100

    Set up Vim environment with directory structure and config symlink

--- vim/install.sh
+++ vim/install.sh
@@ -3,11 +3,7 @@
 # Create Vim directories
 mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 
-#Create a .vimrc file in your home directory
-touch ~/.vimrc
-
 # Create a symbolic link for the Fish configuration
 ln -sfn $DIR/vimrc ~/.vimrc
-
 
 echo "Vim configuration complete."
