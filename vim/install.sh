dc1a8e8276af3ad8098c192d53c0ab9964013df6
Imanol Gomez <imanol@tuta.io>
2023-11-02 20:42:29 +0100

    added vim config

--- vim/install.sh
+++ vim/install.sh
@@ -7,7 +7,7 @@ mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged
 touch ~/.vimrc
 
 # Create a symbolic link for the Fish configuration
-ln -sfn $DIR/vimrc ~~/.vimrc
+ln -sfn $DIR/vimrc ~/.vimrc
 
 
 echo "Vim configuration complete."
